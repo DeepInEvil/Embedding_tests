087f7a0ccc0f4a5159c09bfb1cbb63e07f44da7a
DeepInEvil <deba.kgec@gmail.com>
2018-04-12 00:35:32 +0200

    Embedding Test

--- data.py
+++ data.py
@@ -11,7 +11,7 @@ class Amazon_loader:
     '''
     For loading amazon words
     '''
-    def __init__(self, dom='Home_Kitchen', positive='positive.tsv', negative='negative.tsv', batch_size=64, max_seq_len=100, gpu=True):
+    def __init__(self, dom='Home_Kitchen', positive='positive.tsv', negative='negative.tsv', batch_size=64, max_seq_len=100, gpu=True, emb_file=None, emb_dim=100):
         self.batch_size = batch_size
         self.max_seq_len = max_seq_len
         self.gpu = gpu
@@ -19,19 +19,38 @@ class Amazon_loader:
         self.neg = np.array(pd.read_table(dom + '/' + negative, sep = '\t', header=None)[1])
         self.dat = np.concatenate((self.pos, self.neg))
         self.y = np.concatenate((np.ones(25000), np.zeros(25000)))
-        self.X_tr, self.X_te, self.y_tr, self.y_te = self.create_train_test((self.dat, self.y))
+        self.X_tr, self.X_te, self.y_tr, self.y_te = self.create_train_test((self.dat, self.y), split_size=0.2)
+        self.X_tr, self.X_val, self.y_tr, self.y_val = self.create_train_test((self.X_tr, self.y_tr), split_size=0.125)
         self.vocab = self.create_vocab(self.X_tr)
         np.save(dom + '/' + 'vocab.npy', self.vocab)
         self.X_tr = [[self.getW2Id(self.vocab, w) for w in sent] for sent in self.X_tr]
         self.X_te = [[self.getW2Id(self.vocab, w) for w in sent] for sent in self.X_te]
-        self.train, self.test = {}, {}
+        self.train, self.test, self.valid = {}, {}, {}
         self.train['X'] = self.X_tr
         self.train['y'] = self.y_tr
         self.test['X'] = self.X_te
         self.test['y'] = self.y_te
+        self.valid['X'] = self.X_val
+        self.valid['y'] = self.y_val
 
-        self.emb_dim = 200
+        self.emb_dim = emb_dim
         self.vocab_size = len(self.vocab)
+
+        if emb_file is not None:
+            with open(emb_file, 'r') as f:
+                emb = f.readlines()
+
+        self.vectors = np.zeros((self.vocab_size, self.emb_dim))
+
+        for j in range(1, len(emb)):
+            word = emb[j].split(' \n')[0].split()[0]
+            vec = emb[j].split(' \n')[0].split()[1:]
+            try:
+                self.vectors[self.vocab[word]] = vec
+            except KeyError:
+                continue
+
+        del(emb)
 
     def create_vocab(self, train):
         "create word to id mappings"
@@ -51,10 +70,10 @@ class Amazon_loader:
         except KeyError:
             return w2i['UNK']
 
-    def create_train_test(self, data):
+    def create_train_test(self, data, split_size):
         "create splits"
         X, y = data
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_size, random_state=42, shuffle=True)
         return X_train, X_test, y_train, y_test
 
     def get_iter(self, dataset='train'):
@@ -62,6 +81,8 @@ class Amazon_loader:
             dataset = self.train
         elif dataset == 'test':
             dataset = self.test
+        elif dataset == 'valid':
+            dataset = self.valid
 
         for i in range(0, len(dataset['y']), self.batch_size):
             reviews = dataset['X'][i:i+self.batch_size]
