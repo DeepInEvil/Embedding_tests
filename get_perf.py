17efe8d3a8f0e824a56a7bd05e831b61198b0e2c
DeepInEvil <deba.kgec@gmail.com>
2018-04-13 19:26:28 +0200

    Embedding Test

---
+++ get_perf.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+with open('perf.txt', 'r') as f:
+    perfs = f.readlines()
+
+domains = ['Video_Games', 'Books', 'Toys_Games', 'Tools_Home_Improvement', 'Amazon_Instant_Video', 'Movies_TV', 'Electronics', 'Health',
+           'Shoes', 'Baby', 'Automotive', 'Software', 'Sports_Outdoors', 'Clothing_Accessories', 'Beauty', 'Patio', 'Music',
+           'Pet_Supplies', 'Office_Products', 'Home_Kitchen']
+#word_embeddings = ['embeddings_snap_s256_e15.txt', 'embeddings_snap_s256_e50.txt', 'embeddings_snap_s256_e30.txt',
+                     # 'embeddings_snap_s512_e15.txt', 'embeddings_snap_s128_e15.txt', 'embeddings_snap_s128_e30.txt',
+                     # 'embeddings_snap_s128_e50.txt', 'embeddings_snap_s512_e50.txt', 'embeddings_snap_s512_e30.txt']
+word_embeddings = ['embeddings_snap_s512_e15.txt', 'embeddings_snap_s128_e15.txt', 'embeddings_snap_s128_e30.txt',
+                   'embeddings_snap_s128_e50.txt', 'embeddings_snap_s512_e50.txt', 'embeddings_snap_s512_e30.txt']
+
+it = 0
+perf_dict = defaultdict(list)
+for i, emb in enumerate(word_embeddings):
+    for j, domain in enumerate(domains):
+        it = it + 1.0
+        perf = perfs[it].replace('\n', '').strip()
+        perf_dict[domain].append(perf)
+
+for dom in domains:
+    print ('&'.join(perf_dict[dom]))
+
+
