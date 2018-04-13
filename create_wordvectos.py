6b0a97c825e9891c35ad4816bebee8e17e679537
DeepInEvil <deba.kgec@gmail.com>
2018-04-13 12:14:09 +0200

    Embedding Test

--- create_wordvectos.py
+++ create_wordvectos.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 
 emb_dir = '/data/dchaudhu/ESWC_challenge/Embeddings/'
@@ -9,13 +10,13 @@ word_embeddings = ['embeddings_snap_s512_e15.txt',
 for emb_file in word_embeddings:
     emb_d_f = emb_file.split('.')[0] + '.npy'
     emb_dim = int(emb_file.split('_')[2].split('s')[1])
-    emb_vec = {}
+    emb_vec = defaultdict(float)
     with open(emb_dir + emb_file, 'r') as f:
         emb = f.readlines()
     print ("Loadded the vector, creating the dictionary.....")
     for j in range(1, len(emb)):
         word = emb[j].split('\n')[0].strip().split()[0]
-        vec = emb[j].split('\n')[0].strip().split()[1:]
+        vec = np.array(emb[j].split('\n')[0].strip().split()[1:]).astype(np.float32)
         # try:
         if len(vec) == emb_dim:
             emb_vec[word] = vec
